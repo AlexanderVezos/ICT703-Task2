868b48784542a90063d6d81c89035d3da3981991
AlexanderVezos <vez17@outlook.com>
2025-09-13 12:08:07 +1000

    Implementing Flask and Redis

---
+++ Alexander Vezos 1154053 ICT703 Task 2.py
@@ -0,0 +1,34 @@
+# Imports
+import os
+import ssl # For secure connection to DB
+import redis # Cloud NoSQL Service to store user information
+from flask import Flask, render_template # GUI front end using HTML
+from dotenv import load_dotenv # Securely store DB credentials
+
+# Load secured DB credentials
+load_dotenv() 
+
+# Connect to NoSQL server
+redis_url = f"redis://{os.getenv('REDIS_USERNAME')}:{os.getenv('REDIS_PASSWORD')}@{os.getenv('REDIS_HOST')}:{os.getenv('REDIS_PORT')}/0"
+r = redis.from_url(redis_url, decode_responses=True)
+
+# Initialize  the GUI
+gui = Flask(__name__)
+
+@gui.route('/')
+def home():
+    return render_template('index.html')
+
+# Run the script
+if __name__ == '__main__':
+    print(f"Connecting to Redis at: {os.getenv('REDIS_HOST')}:{os.getenv('REDIS_PORT')}")
+    
+    # Test DB connection on startup
+    try:
+        r.ping()
+        print("Redis connection successful!")
+    except Exception as e:
+        print(f"Redis connection failed: {e}")
+    
+    print("Flask app running at: http://localhost:5000")
+    gui.run(debug=False, host='0.0.0.0', port=5000)
